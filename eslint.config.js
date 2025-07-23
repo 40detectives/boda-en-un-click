6edf6bc5ddb7111f003443166f87eafc819702a3
40detectives <9024463+40detectives@users.noreply.github.com>
2025-07-23 21:46:31 +0200

    chore: setup configuration for Vite + TS + ESLint + React
    You can test and debug ESLint config visually with:
    `npx @eslint/config-inspector@latest`

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,66 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import configPrettier from "eslint-config-prettier/flat";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import { importX } from "eslint-plugin-import-x";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    name: "Vite pre-configured JS & TS - edited",
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      tseslint.configs.eslintRecommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
   },
-])
+  {
+    name: "recommended-import-x",
+    files: ["**/*.{ts,tsx}"],
+    extends: [importX.flatConfigs.recommended, importX.flatConfigs.typescript],
+    languageOptions: {
+      parserOptions: { projectService: true },
+    },
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: ["tsconfig.app.json"],
+        }),
+      ],
+    },
+  },
+  {
+    name: "React + React a11y plugins",
+    extends: [
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      jsxA11y.flatConfigs.recommended,
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    extends: [configPrettier],
+  },
+]);
